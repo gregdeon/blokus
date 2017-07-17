5ddbca874b68dd586d947710db4e14d6c725e6f0
Greg d'Eon <greg.l.deon@gmail.com>
2017-07-17 18:00:13 -0300

    Add proper move checking; make random player check all possible moves

--- game.py
+++ game.py
@@ -28,7 +28,7 @@ class Input(object):
 
     input_error_string = "Error: using base input class"
 
-    def getMove(self, engine):
+    def getMove(self, player, engine):
         """
         """
         raise NotImplementedError(input_error_string)
@@ -37,21 +37,26 @@ class RandomInput(Input):
     """RandomInput players choose random moves (equally distributed over piece
     number, x/y, and rotation/flip)
     """
-    def getMove(self, engine):
+    def getMove(self, player, engine):
         import random
-        piece = random.randint(0, 20)
-        x = random.randint(5, 15)
-        y = random.randint(5, 15)
-        rot = random.randint(0, 3)
-        flip = True if random.random() > 0.5 else False
 
-        return Move(piece, x, y, rot, flip)
+        move_list = []
+        for piece in range(0, 21):
+            for x in range(0, 20):
+                for y in range(0, 20):
+                    for rot in range(0, 4):
+                        for flip in [False, True]:
+                            new_move = Move(piece, x, y, rot, flip)
+                            if engine.checkMoveValid(player, new_move):
+                                move_list.append(new_move)
+        n = random.randint(0, len(move_list) - 1)
+        return move_list[n]
 
 
 class CLIInput(Input):
     """The CLIInput class gets moves from the command line
     """
-    def getMove(self, engine):
+    def getMove(self, player, engine):
         # TODO
         raise NotImplementedError("TODO")
 
@@ -94,21 +99,64 @@ class GameEngine(object):
             print "Board state:"
             self.display.drawBoard(self)
 
-            move = self.inputs[p].getMove(self)
+            move = self.inputs[p].getMove(p, self)
             self._addMove(p, move)
         print "Ending turn %d" % self.turn_num
 
     def checkMoveValid(self, player, move):
         piece = self.piece_list.getPiece(move.piece)
+        attached_corner = False
+        
+        if player == 0:
+            corner_x = self.board_w-1
+            corner_y = 0
+        elif player == 1:
+            corner_x = 0
+            corner_y = 0
+        elif player == 2:
+            corner_x = 0
+            corner_y = self.board_h-1
+        else: #player == 3
+            corner_x = self.board_w-1
+            corner_y = self.board_h-1
+
         for t in range(piece.getNumTiles()):
             (x,y) = piece.getTile(t, move.x, move.y, move.rot, move.flip)
+
+            # Make sure tile in bounds
             if x < 0 or x >= self.board_w or y < 0 or y >= self.board_h:
                 return False
-            # TODO: check for:
-            # - intersecting other pieces
-            # - adjacent to player's pieces
-            # - not attached at corner
-        return True
+
+            # Make sure tile isn't intersecting other pieces
+            if self._state[y][x] != -1:
+                return False
+
+            # Make sure tile isn't next to own pieces
+            if x > 0 and self._state[y][x-1] == player:
+                return False
+            if x < self.board_w-1 and self._state[y][x+1] == player:
+                return False
+            if y > 0 and self._state[y-1][x] == player:
+                return False
+            if y < self.board_h-1 and self._state[y+1][x] == player:
+                return False
+
+            # Check if attached at corner
+            if x > 0 and y > 0 and \
+                self._state[y-1][x-1] == player:
+                attached_corner = True
+            elif x > 0 and y < self.board_h-1 and \
+                self._state[y+1][x-1] == player:
+                attached_corner = True
+            elif x < self.board_w-1 and y < self.board_h-1 and \
+                self._state[y+1][x+1] == player:
+                attached_corner = True
+            elif x < self.board_w-1 and y > 0 and \
+                self._state[y-1][x+1] == player:
+                attached_corner = True
+            elif x == corner_x and y == corner_y:
+                attached_corner = True
+        return attached_corner
 
     def playGame(self):
         while self.turn_num < 10:
